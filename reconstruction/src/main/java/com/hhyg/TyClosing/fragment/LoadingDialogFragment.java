f06ad4f9f77f43a95f9ffaea18b69e9b3451db73
jimmyChen1991 <929488097@qq.com>
2017-06-29 19:29:42 -0700

    mod

--- reconstruction/src/main/java/com/hhyg/TyClosing/fragment/LoadingDialogFragment.java
+++ reconstruction/src/main/java/com/hhyg/TyClosing/fragment/LoadingDialogFragment.java
@@ -1,12 +1,15 @@
 package com.hhyg.TyClosing.fragment;
 
 import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.hhyg.TyClosing.R;
 
@@ -15,6 +18,19 @@ import com.hhyg.TyClosing.R;
  */
 
 public class LoadingDialogFragment extends DialogFragment{
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        Window window = getDialog().getWindow();
+        WindowManager.LayoutParams windowParams = window.getAttributes();
+        windowParams.dimAmount = 0.0f;
+
+        window.setAttributes(windowParams);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
